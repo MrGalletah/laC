2f83b928eac9e42a74e61ba214d4a70359e7ff33
4le17 <4le17@192.168.56.1>
2025-08-31 15:57:05 -0300

    creation of the first branch.

--- src/main/java/com/mycompany/lac/LaC.java
+++ src/main/java/com/mycompany/lac/LaC.java
@@ -11,6 +11,6 @@ package com.mycompany.lac;
 public class LaC {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello capo");
     }
 }
